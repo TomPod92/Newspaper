724155034c8fd52103e9461567b5a5bc840a736d
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2019-07-28 21:18:45 +0200

    Updates

---
+++ precache-manifest.9c35dbe696f27696cdcf974187055ecf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0300593f77a6e02ac8d1f6457d0af370",
+    "url": "/Newspaper/index.html"
+  },
+  {
+    "revision": "02301558679c6091a867",
+    "url": "/Newspaper/static/js/2.f9a15bac.chunk.js"
+  },
+  {
+    "revision": "f56f25cae6cf6120bd93",
+    "url": "/Newspaper/static/js/main.803f64b6.chunk.js"
+  },
+  {
+    "revision": "064fbc6f82b5a09fc9dc",
+    "url": "/Newspaper/static/js/runtime~main.5bf8f918.js"
+  },
+  {
+    "revision": "7ec5c4d7b7a1fa89c2bba75ef4a36bda",
+    "url": "/Newspaper/static/media/11.7ec5c4d7.jpg"
+  },
+  {
+    "revision": "ba872e37882abf31b7c81df1fc55d892",
+    "url": "/Newspaper/static/media/22.ba872e37.jpg"
+  },
+  {
+    "revision": "7ce1fef1dfbacc2c75f8c9940933bd7e",
+    "url": "/Newspaper/static/media/33.7ce1fef1.jpg"
+  },
+  {
+    "revision": "0db103ece74f0c9b5840f75ffb40fbe9",
+    "url": "/Newspaper/static/media/back.0db103ec.jpg"
+  },
+  {
+    "revision": "9fbaf46565f33f08f2a7975ed042c1ce",
+    "url": "/Newspaper/static/media/sprite.9fbaf465.svg"
+  }
+]);
